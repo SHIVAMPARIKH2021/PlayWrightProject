9fb4a25ad7a559a9e2a6f7a58aaa506862560860
SHIVAMPARIKH2021 <96095609+SHIVAMPARIKH2021@users.noreply.github.com>
2023-06-04 16:30:05 -0400

    steps added in test

--- tests/login.ts
+++ tests/login.ts
@@ -2,7 +2,8 @@ import {test,  expect, Locator } from "@playwright/test";
 
 test('Login Test',async ({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-    await page.locator("//input[@name='username']").fill('Admin');
-    await page.locator("//input[@name='password']").fill('admin123');
+    await page.locator("//input[@name='username']").type('Admin');
+    await page.locator("//input[@name='password']").type('admin123');
     await page.locator("//button[text()=' Login ' and @class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']").click();
+    expect(await page.title()).toEqual("OrangeHRM");
 })
